004294b183894bb74e4bf9d199dd1cca2305d7ff
ariqkharisma <ariqkharisma10@gmail.com>
2022-11-07 09:07:51 +0700

    configure database

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,13 @@
-// path: ./my-project/config/database.js
-
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: 'mysql',
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "bcr"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "ariq1234"),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'bcr'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', ''),
+      ssl: env.bool('DATABASE_SSL', false),
     },
-    useNullAsDefault: true,
   },
 });
