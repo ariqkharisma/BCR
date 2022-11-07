a4ba83e1d2820d9be49517ca12891871dfdf5bd8
ariqkharisma <ariqkharisma10@gmail.com>
2022-11-07 18:17:31 +0700

    update database_port

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     client: 'mysql',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
+      port: env.int('DATABASE_PORT', 1337),
       database: env('DATABASE_NAME', 'bcr'),
       user: env('DATABASE_USERNAME', 'root'),
       password: env('DATABASE_PASSWORD', ''),
