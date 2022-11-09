47e49f7ad325ba6f921f84a33e002734c1084c4e
ariqkharisma <ariqkharisma10@gmail.com>
2022-11-10 05:13:19 +0700

    edit package.json

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -1,14 +1,20 @@
-module.exports = {
-  upload: {
-    config: {
-      provider: "strapi-provider-upload-dos",
-      providerOptions: {
-        key: process.env.DO_SPACE_ACCESS_KEY,
-        secret: process.env.DO_SPACE_SECRET_KEY,
-        endpoint: process.env.DO_SPACE_ENDPOINT,
-        space: process.env.DO_SPACE_BUCKET,
-        directory: process.env.DO_SPACE_DIRECTORY,
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+            uploadStream: {
+                folder: env("CLOUDINARY_FOLDER", ""),
+              },
+            delete: {},
+        },
       },
     },
-  },
-};
+    // ...
+  });
